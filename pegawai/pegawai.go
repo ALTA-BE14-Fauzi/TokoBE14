582bff350fb9a3474ee05e89d0886783f6e04f90
Fauzi Sofyan <fauzilax@gmail.com>
2022-12-20 13:23:05 +0700

    buat pegawai.go

--- pegawai/pegawai.go
+++ pegawai/pegawai.go
@@ -1,1 +1,21 @@
 package pegawai
+
+import (
+	"fmt"
+	"strings"
+)
+
+func MenuPegawai(nama string) {
+	inputLogin := "A"
+	for inputLogin != "0" {
+		fmt.Println("================== PEGAWAI ====================") // MENU PEGAWAI------------
+		fmt.Println("Selamat Datang ", strings.ToUpper(nama))
+		fmt.Println("1. Tambahkan Barang Baru")
+		fmt.Println("2. Update")
+		fmt.Println("3. Tambahkan Customer baru")
+		fmt.Println("0. Logout")
+		fmt.Print("Masukkan pilihan : ")
+		fmt.Scanln(&inputLogin)
+		fmt.Println(inputLogin)
+	}
+}
