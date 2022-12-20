29d10e3647410bbf678d92c66d4e960a852877d8
audizzy <zulfeekei@gmail.com>
2022-12-20 13:23:54 +0700

    main.go admin.go

--- admin/admin.go
+++ admin/admin.go
@@ -1,1 +1,89 @@
 package admin
+
+import (
+	"TokoBE14/config"
+	"TokoBE14/user"
+	"fmt"
+	"strings"
+)
+
+func MenuAdmin(nama string) {
+	cfg := config.ReadConfig()
+	conn := config.ConnectSQL(*cfg)
+	authmenu := user.AuthMenu{DB: conn}
+
+	inputLogin := "A"
+	for inputLogin != "0" {
+		fmt.Println("=================== ADMIN =====================") // MENU ADMIN-------------
+		fmt.Println("Selamat Datang ", strings.ToUpper(nama))
+		fmt.Println("1. Tambahkan Pegawai Baru")
+		fmt.Println("2. Hapus Data")
+		fmt.Println("0. Logout")
+		fmt.Print("Masukkan pilihan : ")
+		fmt.Scanln(&inputLogin)
+		if inputLogin == "1" {
+			fmt.Println("========== Tambahkan Pegawai ==========")
+			var newUser user.User
+			fmt.Print("Masukkan nama : ")
+			fmt.Scanln(&newUser.Nama)
+			fmt.Print("Masukkan password : ")
+			fmt.Scanln(&newUser.Password)
+			res, err := authmenu.Register(newUser)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			if res {
+				fmt.Println("Sukses mendaftarkan Pegawai")
+			} else {
+				fmt.Println("Gagal mendaftarkann Pegawai")
+			}
+		} else if inputLogin == "2" {
+			inputHapus := "A"
+			for inputHapus != "0" {
+
+				fmt.Println("========== Hapus ==========")
+				fmt.Println("1. Hapus Pegawai")
+				fmt.Println("2. Hapus Items")
+				fmt.Println("3. Hapus Transaksi")
+				fmt.Println("4. Hapus Customers")
+				fmt.Print("Masukan Pilihan : ")
+				fmt.Scanln(&inputHapus)
+				fmt.Println(inputHapus)
+				if inputHapus == "1" {
+					fmt.Println("============== Hapus Pegawai ==============")
+					fmt.Println("List Pegawai Toko :")
+					fmt.Println("0. Batal")
+					fmt.Println("1. Syahrini")
+					fmt.Println("Pilih Pegawai yang akan dihapus (0-9) : ")
+
+				}
+				if inputHapus == "2" {
+					fmt.Println("============== Hapus Items ==============")
+					fmt.Println("List Items :")
+					fmt.Println("0. Batal")
+					fmt.Println("1. Syahrini")
+					fmt.Println("Pilih Pegawai yang akan dihapus (0-9) : ")
+				}
+				if inputHapus == "3" {
+					fmt.Println("============== Hapus Transaksi ==============")
+					fmt.Println("List Transaksi :")
+					fmt.Println("0. Batal")
+					fmt.Println("1. Indomie")
+					fmt.Println("Pilih Pegawai yang akan dihapus (0-9) : ")
+				}
+				if inputHapus == "4" {
+					fmt.Println("============== Hapus Customers ==============")
+					fmt.Println("List Customers :")
+					fmt.Println("0. Batal")
+					fmt.Println("1. Syahrini")
+					fmt.Println("Pilih Customers yang akan dihapus (0-9) : ")
+				}
+
+			}
+
+		} else if inputLogin != "0" && inputLogin != "1" && inputLogin != "2" {
+			fmt.Println("Input yang anda masukan tidak cocok, Silahkan sesuai pilihan menu")
+		}
+	}
+	// fmt.Println(inputLogin)
+}
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"TokoBE14/admin"
+	"TokoBE14/config"
+	"TokoBE14/pegawai"
+	"TokoBE14/user"
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Helloeasfad")
+	cfg := config.ReadConfig()
+	conn := config.ConnectSQL(*cfg)
+	authmenu := user.AuthMenu{DB: conn}
+	var inputHome string
+	fmt.Println(reflect.ValueOf(authmenu).Kind())
+	for inputHome != "0" {
+		// MENU AWAL
+		fmt.Println("============= Welcome to TOKO BE 14 =============")
+		fmt.Println("1. Login")
+		fmt.Println("0. Exit")
+		fmt.Print("Masukan Pilihan : ")
+		fmt.Scanln(&inputHome)
+		// fmt.Println(string(inputHome))
+		if inputHome == "1" {
+			var inputNama, inputPassword string
+			fmt.Println("============= Log In =============")
+			fmt.Print("Masukkan Nama : ")
+			fmt.Scanln(&inputNama)
+			fmt.Print("Masukkan Password : ")
+			fmt.Scanln(&inputPassword)
+			res, err := authmenu.Login(inputNama, inputPassword)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			// fmt.Println(res.Role)
+			if res.Role == 1 {
+				admin.MenuAdmin(res.Nama)
+			}
+			if res.Role == 2 {
+				pegawai.MenuPegawai(res.Nama)
+			}
+		} else if inputHome != "0" {
+			fmt.Println("Anda Hanya dapat Menginput 1. Login dan 0. Exit")
+		}
+	}
 }
