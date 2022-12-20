eb1c1adabe95181bd0353d2b49bc8b4ed426dd0a
Fauzi Sofyan <fauzilax@gmail.com>
2022-12-20 13:16:32 +0700

    masukan pegawai

--- config/config.go
+++ config/config.go
@@ -14,38 +14,34 @@ type Config struct {
 	DBUser string
 	DBPass string
 	DBHost string
-	DBPort int
 	DBName string
+	DBPort int
 }
 
 func ReadConfig() *Config {
 	err := godotenv.Load("local.env")
 	if err != nil {
-		fmt.Println("Error saat baca env", err.Error())
+		fmt.Println("Error saat baca file envv", err.Error())
 		return nil
 	}
-
-	var res Config
-	res.DBUser = os.Getenv("DBUSER")
-	res.DBPass = os.Getenv("DBPASS")
-	res.DBHost = os.Getenv("DBHOST")
-	readData := os.Getenv("DBPORT")
-	res.DBPort, err = strconv.Atoi(readData)
+	var read Config
+	read.DBUser = os.Getenv("DBUSER")
+	read.DBPass = os.Getenv("DBPASS")
+	read.DBHost = os.Getenv("DBHOST")
+	read.DBName = os.Getenv("DBNAME")
+	convData := os.Getenv("DBPORT")
+	read.DBPort, err = strconv.Atoi(convData)
 	if err != nil {
-		fmt.Println("Error saat convert", err.Error())
-		return nil
+		fmt.Println("Error Saat Conversi Port String ke Int", err.Error())
 	}
-	res.DBName = os.Getenv("DBNAME")
-	return &res
+	return &read
 }
 
 func ConnectSQL(c Config) *sql.DB {
-	// format source username:password@tcp(host:port)/databaseName
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", c.DBUser, c.DBPass, c.DBHost, c.DBPort, c.DBName)
-	db, err := sql.Open("mysql", dsn)
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", c.DBUser, c.DBPass, c.DBHost, c.DBPort, c.DBName)
+	db, err := sql.Open("mysql", dns)
 	if err != nil {
-		fmt.Println("Terjadi error", err.Error())
+		fmt.Println("Error Saat Connect Ke SQL", err.Error())
 	}
-
 	return db
 }
